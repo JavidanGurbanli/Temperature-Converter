0609a7a407d1808d5e910df1a6ef37e91df8b717
cavidan <cavidanqurbanli2003@gmail.com>
2022-06-21 20:57:24 +0400

    Temperature converter

---
+++ temperature.js
@@ -0,0 +1,24 @@
+const fInput = document.getElementById("fahrenheit");
+const cInput = document.getElementById("celcius");
+const kInput = document.getElementById("kelvin");
+const inputs = document.getElementsByTagName("input");
+for(let i = 0; i < inputs.length; i++){
+    let input = inputs[i];
+    input.addEventListener("input",function(e){
+      let value = parseFloat(e.target.value);
+     switch(e.target.name){
+        case "celcius":
+                fInput.value = (value * 1.8) + 32;
+                kInput.value = value + 273.15;
+                break;
+            case "fahrenheit":
+                cInput.value = (value - 32) / 1.8;
+                kInput.value = ((value - 32) / 1.8) + 273.15;
+                break;
+            case "kelvin":
+                cInput.value = value - 273.15;
+                fInput.value = ((value - 273.15) * 1.8) + 32;
+                break;
+     }
+    });
+}
